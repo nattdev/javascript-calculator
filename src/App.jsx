d4f3e81e4dad9037ec9dbbd4ea47ab892b807c9f
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-01-19 02:20:37 -0500

    Add functionality to dark mode element

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
 import Calculator from './components/Calculator';
+import darkModeIcon from './assets/dark-mode.svg';
+import lightModeIcon from './assets/light-mode.svg';
 
 function App() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (isDarkMode === false) {
+      document.querySelector('html').classList.add('dark')
+    } else {
+      document.querySelector('html').classList.remove('dark')
+    }
+  }, [isDarkMode]);
 
   return (
     <>
       <Calculator />
+      <div onClick={() => {setIsDarkMode(!isDarkMode)}} className="absolute right-0
+      bottom-0 bg-white rounded-full p-4 m-6 cursor-pointer"><img src={isDarkMode ? darkModeIcon : lightModeIcon}></img></div>
     </>
   )
 }
